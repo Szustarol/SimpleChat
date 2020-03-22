749eeccba11597efe4f5f41fe3d77be5035449e8
Szustarol <szustkarol@gmail.com>
2020-03-22 11:21:31 +0100

    Removed unnecessary functions

--- main.c
+++ main.c
@@ -6,6 +6,15 @@
 #include "CLIENT/clientloop.h"
 #include "communicate.h"
 
+/*
+ the server is always running, but is not always active - when it receives SIGUSR1,
+ it sets up the socket and binds it. If the user never hosts, the server does nothing.
+ IF the server receives SIGUSR2, it means that the user is a host, and it want's to stop
+ hosting, so the server should close the socket, but still be ready to start when asked to.
+ SIGTERM is sent when the user closes the application, the server sends the quit message to 
+ all the connected clients, closes all active sockets and returns.
+*/
+
 int main(int argc, char *argv[]) {
 	program_childPID = fork();
 	if(program_childPID == 0){
